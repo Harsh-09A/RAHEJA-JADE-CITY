df6cd913d05f4bff175dca4fa1ff3f54052f2ae6
Harsh-09A <harsh.autowebbed@gmail.com>
2026-01-19 11:10:33 +0530

    CC Email Added

--- js/mail.js
+++ js/mail.js
@@ -35,6 +35,8 @@ const handleFormSubmit = async (
     website_url: window.location.href,
     to_email: "aristosocialmedia9@gmail.com",
     company_name: "K Raheja Jade City",
+    date: new Date().toLocaleDateString("en-GB"),
+    cc_email: "cbWR9lQS-197a6H60@v1-incoming-leads.privyr.com",
   };
 
   // Send Form
